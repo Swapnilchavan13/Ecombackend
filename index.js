2643944132aecfc4ac3a12c2e15c9ade866204fd
swapnil chavan <chavanswapnil822@gmail.com>
2023-11-28 13:17:18 +0530

    updated code with address updateing code

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-require('dotenv').config();
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 const port = 3005;
 
-const Productdata = require('./models/productdata')
-const Userdata = require('./models/User')
+const Productdata = require("./models/productdata");
+const Userdata = require("./models/User");
 
 // MongoDB Connection
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 
 const connectDB = async () => {
   try {
@@ -28,17 +28,17 @@ const connectDB = async () => {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: "*" }));
 
 // Simple get request
-app.get('/', (req, res) => {
-  res.send('Hello Ecom Admin');
+app.get("/", (req, res) => {
+  res.send("Hello Ecom Admin");
 });
 
-
 // Registerd Uaer Data
-app.post('/userdata', async (req, res) => {
-  const { username, usernumber, userpassword, useraddress, userorder } = req.body;
+app.post("/userdata", async (req, res) => {
+  const { username, usernumber, userpassword, useraddress, userorder } =
+    req.body;
 
   try {
     // Check if the user already exists based on username and mobile number
@@ -49,7 +49,7 @@ app.post('/userdata', async (req, res) => {
       existingUser.useraddress = useraddress;
       await existingUser.save();
       console.log("User Address Updated");
-      res.status(200).json({ message: 'User Address Updated' });
+      res.status(200).json({ message: "User Address Updated" });
     } else {
       // If the user doesn't exist, create a new entry
       const newData = new Userdata({
@@ -57,23 +57,22 @@ app.post('/userdata', async (req, res) => {
         usernumber,
         userpassword,
         useraddress,
-        userorder
+        userorder,
       });
       await newData.save();
       console.log("New User Data Saved");
-      res.status(200).json({ message: 'New User Data saved' });
+      res.status(200).json({ message: "New User Data saved" });
     }
   } catch (err) {
     console.error("Error Saving/Updating User Data", err);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-
-//Patch request 
+//Patch request
 
 // Update User Address Data
-app.patch('/userdata/:id', async (req, res) => {
+app.patch("/userdata/:id", async (req, res) => {
   const userId = req.params.id;
   const { useraddress } = req.body;
 
@@ -82,7 +81,7 @@ app.patch('/userdata/:id', async (req, res) => {
     const existingUser = await Userdata.findById(userId);
 
     if (!existingUser) {
-      return res.status(404).json({ error: 'User not found' });
+      return res.status(404).json({ error: "User not found" });
     }
 
     // Update the user's address
@@ -90,27 +89,25 @@ app.patch('/userdata/:id', async (req, res) => {
     await existingUser.save();
 
     console.log("User Address Updated");
-    res.status(200).json({ message: 'User Address Updated' });
+    res.status(200).json({ message: "User Address Updated" });
   } catch (err) {
     console.error("Error Updating User Address", err);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-
 // Assuming your existing code to fetch all user data looks like this
-app.get('/userdata', async (req, res) => {
+app.get("/userdata", async (req, res) => {
   try {
     const userData = await Userdata.find();
     res.json(userData);
   } catch (error) {
-    console.error('Error fetching user data:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error fetching user data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-// Modify the endpoint to accept query parameters for username and usernumber
-app.get('/userdata', async (req, res) => {
+app.get("/userdata", async (req, res) => {
   const { username, usernumber } = req.query;
 
   try {
@@ -125,70 +122,82 @@ app.get('/userdata', async (req, res) => {
     }
 
     const userData = await Userdata.find(query);
-    res.json(userData);
+
+    // Check if both username and usernumber are provided
+    if (username && usernumber && userData.length === 1) {
+      res.json(userData[0]); // Return the single matching user
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
   } catch (error) {
-    console.error('Error fetching user data:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error fetching user data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-
-app.get('/allusers', async (req, res) => {
-try{
-  const allusers = await Userdata.find()
-  res.status(200).json(allusers)
-}catch(err){
-  console.error('Error Fetching Data', err)
-  res.status(500).json({message:"Internam Server Error"})
-}
+app.get("/allusers", async (req, res) => {
+  try {
+    const allusers = await Userdata.find();
+    res.status(200).json(allusers);
+  } catch (err) {
+    console.error("Error Fetching Data", err);
+    res.status(500).json({ message: "Internam Server Error" });
+  }
 });
 
 // Adding Products to the Site
-app.post('/addproduct', async (req, res) => {
-    const {producttype, productname, productimage, productprice, productdiscount, productquantity} = req.body;
+app.post("/addproduct", async (req, res) => {
+  const {
+    producttype,
+    productname,
+    productimage,
+    productprice,
+    productdiscount,
+    productquantity,
+  } = req.body;
 
-    try{
-        const newData = new Productdata({
-            producttype,
-            productname,
-            productimage,
-            productprice,
-            productdiscount,
-            productquantity
-        })
-        await newData.save()
-        console.log("Product Data Saved.")
-        res.status(200).json({message: "Product Data Saved"})
-    } catch(err) {
-        console.error("Error Saving Data", err)
-    }
+  try {
+    const newData = new Productdata({
+      producttype,
+      productname,
+      productimage,
+      productprice,
+      productdiscount,
+      productquantity,
+    });
+    await newData.save();
+    console.log("Product Data Saved.");
+    res.status(200).json({ message: "Product Data Saved" });
+  } catch (err) {
+    console.error("Error Saving Data", err);
+  }
 });
 
 //To Get All Data
-app.get('/allproducts', async (req, res) => {
+app.get("/allproducts", async (req, res) => {
   try {
     const allProducts = await Productdata.find();
     res.status(200).json(allProducts);
   } catch (err) {
-    console.error('Error Fetching Data', err);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error Fetching Data", err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 //Delete The Product
-app.delete('/deleteproduct/:productId', async (req, res) => {
+app.delete("/deleteproduct/:productId", async (req, res) => {
   const productId = req.params.productId;
 
   try {
     const deletedProduct = await Productdata.findByIdAndDelete(productId);
 
     if (!deletedProduct) {
-      return res.status(404).json({ error: 'Product not found' });
+      return res.status(404).json({ error: "Product not found" });
     }
-    res.status(200).json({ message: 'Product deleted successfully' });
+    res.status(200).json({ message: "Product deleted successfully" });
   } catch (err) {
-    console.error('Error Deleting Product', err);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error Deleting Product", err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
